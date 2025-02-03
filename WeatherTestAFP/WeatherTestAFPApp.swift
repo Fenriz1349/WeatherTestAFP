0e9c41f5709b9022d47b8754f8f1be1e0326fb15
Julien Cotte <Julien.Cotte-CIC@ibm.com>
2025-02-03 18:16:42 +0100

    Initial Commit

---
+++ WeatherTestAFP/WeatherTestAFPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTestAFPApp.swift
+//  WeatherTestAFP
+//
+//  Created by Julien Cotte on 03/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTestAFPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
